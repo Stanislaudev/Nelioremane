6578a8ff85e9f014281a6f13d60fdeb879b21bb8
StanislauEscondido <82816316+Stanislau00@users.noreply.github.com>
2024-10-16 04:27:23 +0200

    the PENULTIMO

---
+++ googlesheet.js
@@ -0,0 +1,14 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx5d_iIs6t0fFaVF3DnpHYyGFvbtWj-mKrAu6duB83j6KY3xSuZHsPP6Ni2eYKOs2e6mg/exec'
+
+
+const form = document.forms['contact-form']
+
+
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => ("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
